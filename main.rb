94cc2be714575536c4a0f51fa9f2e9ba30c0be5c
mklarman <mklarman@hotmail.com>
2018-02-05 17:42:32 -0500

    ruby exercises

---
+++ main.rb
@@ -0,0 +1,76 @@
+# def text_add (x)
+# 	puts x + " " + 'only in America'
+# end
+# text_add ('I steal ')
+
+# def math_time(x,y)
+# 	puts (x + y)/3
+# end
+# math_time(7.0,4.0)
+
+
+# myself = {
+# 	'name' => 'Michael',
+# 	'birthday' => 'Oct. 27th',
+# 	'interests' => ['poker', 'NFL', 'napping', 'doing hard core drugs']
+
+# }
+# puts myself['interests']
+
+# puts 'pick a door 1, 2, or 3'
+
+# number = gets.chomp.to_i
+
+# if number == 1
+# 	puts 'You won a car'
+# elsif number == 2
+# 	puts 'You won a washer and dryer'
+# else number == 3
+# 	puts 'You won herpes'
+# end
+
+# array = ['billy', 'ralph', 'alli']
+# for i in array
+# 	puts i + ' is my friend'
+# end
+
+# array = [-2, -5, -90, -78, -900]
+# max = array[0]
+
+# for i in array
+# if i>max
+# 	max = i
+#  end
+# end
+# puts max
+
+
+
+# array1 = ['name', 'D.O.B.', 'City']
+# array2 = ['mike', 'Oct.27th', 'Philly']
+# puts Hash[array1.zip(array2)]
+
+# for i in 1..100
+# 	if i % 15 == 0
+# 		puts 'FizzBuzz'
+# 	elsif i % 3 == 0
+# 		puts 'Fizz'
+# 	elsif i % 5 == 0
+# 		puts 'Buzz'
+# 	else
+# 		puts i
+# 	end
+# end
+
+bottles = 99
+while bottles != 0
+puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottless of beer.'
+bottles = bottles - 1
+puts 'Take one and pass it around, ' + bottles.to_s + ' of beer on the wall.'
+puts ''
+end
+
+
+
+
+		
